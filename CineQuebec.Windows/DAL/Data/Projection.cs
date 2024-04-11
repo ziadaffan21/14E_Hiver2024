d1fbf04e9f533f0bee65d21229fbd31e09c7b430
Champlapin <105014878+Champlapin@users.noreply.github.com>
2024-04-11 12:32:54 -0400

    modification d'interface

--- CineQuebec.Windows/DAL/Data/Projection.cs
+++ CineQuebec.Windows/DAL/Data/Projection.cs
@@ -78,7 +78,7 @@ namespace CineQuebec.Windows.DAL.Data
         public override string ToString()
         {
 
-            return $"{Utils.Utils.GetMoisNom(Date)} {Date.Hour:00}:{Date.Minute:00}";
+            return $"{Date.Day} {Utils.Utils.GetMoisNom(Date)} {Date.Year} {Date.Hour:00}:{Date.Minute:00}";
         }
         #endregion
     }
