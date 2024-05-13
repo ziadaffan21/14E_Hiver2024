6b0dbe0d90b2aa04b73c528a032c2863e638d7ac
Pierre Junior Sibafeu Wafeu <sibafwafpj@gmail.com>
2024-05-13 03:41:31 -0400

    :hammer: Création de la fenetre et viewModelRecompense

---
+++ CineQuebec.Windows.Tests.UI/ViewModel/AjoutUserModelTest.cs
@@ -0,0 +1,17 @@
+﻿using CineQuebec.Windows.DAL.Data;
+using CineQuebec.Windows.DAL.InterfacesRepositorie;
+using CineQuebec.Windows.DAL.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineQuebec.Windows.Tests.UI.ViewModel
+{
+    public class AjoutUserModelTest
+    {
+
+    }
+}
